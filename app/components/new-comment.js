5ec3c7c69582a3bceea5c08d349e928fa6079b67
Michelle Poterek and Nathan Stewart <nathan@>
2017-08-10 09:11:13 -0700

    add comments section to index with ability to add new comments

---
+++ app/components/new-comment.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewComment: false,
+  actions: {
+    commentFormShow() {
+      this.set('addNewComment', true);
+    },
+    saveComment() {
+      var params = {
+        name: this.get('name') ? this.get('name') : "Anonymous",
+        word: this.get('word') ? this.get('word') : "No comment..."
+      };
+      this.set('addNewComment', false);
+      this.sendAction('saveComment', params);
+    }
+  }
+});
